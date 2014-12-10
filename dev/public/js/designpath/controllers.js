03f85d25cec83c551638fe6f9b00118609228d5f
Mirai Akagawa <mirai418@me.com>
2014-12-10 06:44:33 -0500

    restructuring output page

--- dev/public/js/designpath/controllers.js
+++ dev/public/js/designpath/controllers.js
@@ -36,6 +36,29 @@ angular.module("designpathApp")
 
   $scope.outcome = QuizService.getOutcome();
 
+  $scope.circles = [
+    { key: 'Research', keyPlace: 'NW', diameter: 380, top: 100, left: 0 },
+    { key: 'UX Macro', keyPlace: 'NW', diameter: 290, top: 103, left: 150 },
+    { key: 'UX Micro', keyPlace: 'NE', diameter: 290, top: 103, left: 190 },
+    { key: 'Visual', keyPlace: 'NE', diameter: 290, top: 0, left: 270 },
+    { key: 'Implementation', keyPlace: 'SE', diameter: 300, top: 200, left: 265 },
+    { key: 'Business', keyPlace: 'SW', diameter: 370, top: 150, left: 180 }
+  ];
+
+  $scope.dots = [
+    { x: 210, y: 220, title:'abstract'},
+    { x: 210, y: 275, title:'people'},
+    { x: 285, y: 140, title:'diagnostic'},
+    { x: 285, y: 360, title:'analytical'},
+    { x: 305, y: 80, title:'design'},
+    { x: 305, y: 420, title:'number'},
+    { x: 325, y: 140, title:'intuition'},
+    { x: 325, y: 360, title:'sensing'},
+    { x: 410, y: 220, title:'concrete'},
+    { x: 410, y: 275, title:'information'}
+  ];
+
+
 }])
 
 .controller("AboutCtrl", [ "$scope", function ($scope) {
@@ -51,8 +74,8 @@ angular.module("designpathApp")
 
   $scope.circles = [
     { key: 'Research', keyPlace: 'NW', diameter: 380, top: 100, left: 0 },
-    { key: 'Macro', keyPlace: 'NW', diameter: 290, top: 103, left: 150 },
-    { key: 'Micro', keyPlace: 'NE', diameter: 290, top: 103, left: 190 },
+    { key: 'UX Macro', keyPlace: 'NW', diameter: 290, top: 103, left: 150 },
+    { key: 'UX Micro', keyPlace: 'NE', diameter: 290, top: 103, left: 190 },
     { key: 'Visual', keyPlace: 'NE', diameter: 290, top: 0, left: 270 },
     { key: 'Implementation', keyPlace: 'SE', diameter: 300, top: 200, left: 265 },
     { key: 'Business', keyPlace: 'SW', diameter: 370, top: 150, left: 180 }
